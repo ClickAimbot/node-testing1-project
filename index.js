135973f829e6ccbee9c913f9a47c3bf3ff90ca58
Austin <austin.amaral3@gmail.com>
2023-11-05 20:03:48 -0800

    Done

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -135,7 +136,16 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const miles = this.tank * this.mpg
+    if (distance <= miles) {
+      this.odometer += distance
+      this.tank -= (distance / this.mpg)
+      return this.odometer
+    } else {
+      this.odometer += miles
+      this.tank = 0
+      return this.odometer
+    }
   }
 
   /**
@@ -150,7 +160,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank += gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
@@ -168,7 +184,13 @@ class Car {
  * })
  */
 function isEvenNumberAsync(number) {
-  // ✨ implement
+  return new Promise((resolve) => {
+    if (number % 2 === 0) {
+      resolve(true)
+    } else {
+      resolve(false)
+    }
+  })
 }
 
 module.exports = {
